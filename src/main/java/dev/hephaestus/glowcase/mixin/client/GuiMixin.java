3a2b169b08bb535378923bfa5c5a8e0f0a0db9e9
Ampflower 🌺 <git@ampflower.gay>
2026-03-23 12:28:37 -0700

    fix: renderCrosshair is to be extracted instead.

--- src/main/java/dev/hephaestus/glowcase/mixin/client/GuiMixin.java
+++ src/main/java/dev/hephaestus/glowcase/mixin/client/GuiMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(Gui.class)
 public abstract class GuiMixin {
-    @ModifyArg(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIII)V", ordinal = 0), index = 1)
+	@ModifyArg(method = "extractCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphicsExtractor;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIII)V", ordinal = 0), index = 1)
     public Identifier usePickupCrosshair(Identifier original) {
         if (Minecraft.getInstance().player != null && Minecraft.getInstance().level != null && Minecraft.getInstance().hitResult instanceof BlockHitResult bhr && Minecraft.getInstance().level.getBlockState(bhr.getBlockPos()).is(Glowcase.ITEM_PROVIDER_BLOCK.get()) && Glowcase.ITEM_PROVIDER_BLOCK.get().canPickup(Minecraft.getInstance().player, bhr.getBlockPos())) {
             return GlowcaseClient.PROVIDER_CROSSHAIR_TEXTURE;
