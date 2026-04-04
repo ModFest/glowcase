477ce6ce0812f45fce6c7ed1f12a4c1e24358db4
LunaFox <awakenedredstone@gmail.com>
2026-04-04 20:36:29 -0300

    fix: Use an existing sodium version

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,8 @@ repositories {
 	}
 
 	maven {
-		name = "CaffeineMC Maven (Snapshots)"
-		url = uri("https://maven.caffeinemc.net/snapshots")
+		name = "CaffeineMC Maven (Releases)"
+		url = uri("https://maven.caffeinemc.net/releases")
 	}
 
 	mavenLocal()
