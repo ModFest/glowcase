93ddbe51ace1834cefe2538faea9771b3d3d43de
LunaFox <awakenedredstone@gmail.com>
2026-04-27 16:45:34 -0300

    cleanup: Cleanup SizeConstants

--- src/main/java/dev/hephaestus/glowcase/util/SizeConstants.java
+++ src/main/java/dev/hephaestus/glowcase/util/SizeConstants.java
@@ -1,12 +1,8 @@
 package dev.hephaestus.glowcase.util;
 
 public class SizeConstants {
-	/**
-	 * 1 Kibi
-	 */
-	public static final int Ki = 1024;
-	/**
-	 * 1 Mebi
-	 */
-	public static final int Mi = 1024 * Ki; // 1 Mebi
+	/// 1 Kibi
+	public static final int Ki = 1 << 10;
+	/// 1 Mebi
+	public static final int Mi = 1 << 20; // 1 Mebi
 }
